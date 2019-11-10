d5dc9bfe9d862b4f6e1e9b770b1ab43d2092bc24
Deepak <deepak@apptino.com>
2019-11-10 18:42:17 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import {CardList} from './components/card-list/card-list.component'; 
+import {SearchBox} from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super(); // it calls the constructor method on the component class
+
+    this.state = {
+      monsters: [],
+      searchField : ''
+    };
+
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters : users}))
+  }
+
+  handleChange = (e) => {
+      this.setState({searchField: e.target.value});
+  }
+render() {
+  const { monsters, searchField } = this.state; 
+  const filteredMonsters = monsters.filter(monster => 
+    monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder = 'Search Monsters'
+          // handleChange = {e => { //synthetic event
+          //   this.setState({searchField: e.target.value});
+          //   }}
+          handleChange = {this.handleChange}
+        />
+      {/* <CardList monsters={this.state.monsters}>
+      
+      </CardList> */}
+      <CardList monsters={filteredMonsters}>
+      
+      </CardList>
+
     </div>
   );
+}
 }
 
 export default App;
