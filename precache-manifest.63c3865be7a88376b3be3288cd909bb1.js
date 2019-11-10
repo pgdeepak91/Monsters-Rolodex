abaad33d20effd39f357efb3ace5553cfa1a1fa6
Deepak <pgdeepak91@gmail.com>
2019-11-10 22:44:14 +0530

    Updates

---
+++ precache-manifest.63c3865be7a88376b3be3288cd909bb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c35c1f81c93b9dbeb48a6463969acf6",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "1585114c212350975138",
+    "url": "/Monsters-Rolodex/static/css/main.27a14013.chunk.css"
+  },
+  {
+    "revision": "331a6c9f26a1f158523b",
+    "url": "/Monsters-Rolodex/static/js/2.f641515a.chunk.js"
+  },
+  {
+    "revision": "1585114c212350975138",
+    "url": "/Monsters-Rolodex/static/js/main.7590cf51.chunk.js"
+  },
+  {
+    "revision": "06979d115b047ced14a3",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.fc7ec9bc.js"
+  }
+]);
